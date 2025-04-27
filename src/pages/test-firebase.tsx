9ab65537216ebf6456bf79542f7c7cda8c1550d0
Diego Marinho <diegodonald@gmail.com>
2025-04-27 15:44:11 -0300

    Teste de conectividade com o firebase

---
+++ src/pages/test-firebase.tsx
@@ -0,0 +1,20 @@
+// src/pages/test-firebase.tsx
+import React, { useEffect } from "react";
+import { auth } from "@/lib/firebase";
+
+const TestFirebase: React.FC = () => {
+  useEffect(() => {
+    // Verifica se a configuração do Firebase foi corretamente inicializada.
+    console.log("Objeto auth do Firebase:", auth);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-center text-2xl font-bold">
+        Firebase está configurado corretamente!
+      </h1>
+    </div>
+  );
+};
+
+export default TestFirebase;
