f7bf24651a499c029dcae26783b9a6dc47d9cf09
Diego Marinho <diegodonald@gmail.com>
2025-04-28 23:17:08 -0300

    Criação da função de visualizar a senha na tela de Login e adicionado campo adicional para confirmação de senha no signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,15 +11,22 @@ import Footer from "@/components/Footer";
 const Signup: React.FC = () => {
   // Estado para armazenar o nome do usuário
   const [name, setName] = useState("");
-  // Estados para email, senha e possíveis erros
+  // Estados para email, senha, confirmação e possíveis erros
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
 
   // Função para tratar o envio do formulário
   const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("As senhas não coincidem.");
+      return;
+    }
+
     try {
       // Cria o usuário no Firebase Auth e obtém as credenciais
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -30,7 +37,7 @@ const Signup: React.FC = () => {
         name, // salva o nome do usuário
         email: user.email,
         createdAt: new Date(),
-        role: "user" // define o role padrão para "user"
+        role: "user", // define o role padrão para "user"
       });
 
       // Redireciona para a página inicial ou para outra rota desejada
@@ -54,6 +61,7 @@ const Signup: React.FC = () => {
         <form onSubmit={handleSignup} className="bg-gray-900 p-6 rounded shadow-md w-full max-w-sm">
           <h2 className="text-2xl mb-4 text-center">Cadastre-se</h2>
           {error && <p className="text-red-500 mb-4 text-center">{error}</p>}
+          
           {/* Campo para o nome */}
           <input
             type="text"
@@ -63,6 +71,7 @@ const Signup: React.FC = () => {
             required
             className="bg-gray-800 border border-gray-700 px-3 py-2 w-full mb-4 rounded focus:outline-none focus:border-yellow-500"
           />
+          {/* Campo para o email */}
           <input
             type="email"
             placeholder="Digite seu email"
@@ -71,6 +80,7 @@ const Signup: React.FC = () => {
             required
             className="bg-gray-800 border border-gray-700 px-3 py-2 w-full mb-4 rounded focus:outline-none focus:border-yellow-500"
           />
+          {/* Campo para a senha */}
           <input
             type="password"
             placeholder="Digite sua senha"
@@ -78,6 +88,15 @@ const Signup: React.FC = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
             className="bg-gray-800 border border-gray-700 px-3 py-2 w-full mb-4 rounded focus:outline-none focus:border-yellow-500"
+          />
+          {/* Campo para a confirmação de senha */}
+          <input
+            type="password"
+            placeholder="Confirme sua senha"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            className="bg-gray-800 border border-gray-700 px-3 py-2 w-full mb-6 rounded focus:outline-none focus:border-yellow-500"
           />
           <button
             type="submit"
